d5674b81bbea559c7ddb20877228a785ce5eaf07
Doowankim <rlaendhks11@gmail.com>
2020-04-24 12:13:14 +0900

    feat: post api server

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const postModel = require('../../model/posting');
 const passport = require('passport');
 const authCheck = passport.authenticate('jwt', { session: false });
 
+
 // @route POST localhost:3040/posts
 // @desc Tests posts route
 // @access Private
@@ -19,6 +20,41 @@ router.post('/', authCheck, (req, res) => {
     newPost
         .save()
         .then(post => res.json(post))
+        .catch(err => res.json(err));
+});
+
+// @route GET localhost:3040/posts/total
+// @desc get posts
+// @access Private & Public
+router.get('/total', (req, res) => {
+    postModel
+        .find()
+        .sort({ date: -1 }) //날짜에 맞춰서 최신순이 위로 게시된다
+        .then(posts => {
+            res.json({
+                count: posts.length,
+                posts: posts
+            });
+        })
+        .catch(err => res.json(err));
+});
+
+// @route GET localhost:3040/posts/:postId
+// @desc get detail posts
+// @access Private
+router.get('/:postId', authCheck, (req, res) => {
+    const id = req.params.postId;
+    postModel
+        .findById({_id: id})
+        .then(post => {
+            if(!post){
+                return res.json({
+                    msg: 'There is no posts for this user'
+                });
+            } else{
+                res.json(post);
+            }
+        })
         .catch(err => res.json(err));
 });
 
--- validation/post.js
+++ validation/post.js
@@ -5,22 +5,21 @@ module.exports = function validatePostInput(data){
     let errors = {};
 
     data.text = !is_Empty(data.text) ? data.text : '';
-    data.title = !is_Empty(data.title) ? data.title : '';
 
     if(!Validator.isLength(data.text, { min: 10, max: 300 })){
-        errors.text = 'Text must be between 10 and 300 characters'
+        errors.text = 'Post must be between 10 and 300 characters'
     }
 
-    if(!Validator.isLength(data.title, { min: 2, max: 300 })){
-        errors.title = "Title must be between 2 and 300 characters"
+    if(!Validator.isLength(data.title, { min: 10, max: 30000 })){
+        errors.title = 'Post must be between 10 and 30000 characters'
     }
 
     if(Validator.isEmpty(data.text)){
-        errors.text = 'Text field is required'
+        errors.text = 'Text is required'
     }
 
     if(Validator.isEmpty(data.title)){
-        errors.title = 'Title Field is required'
+        errors.title = 'Title is required'
     }
 
     return {
