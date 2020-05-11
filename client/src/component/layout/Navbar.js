cb1033ecc2651b4e4c6c2f649cbbb927a6caedb7
Doowankim <rlaendhks11@gmail.com>
2020-05-11 21:47:12 +0900

    fix: Navbar Mypage

--- client/src/component/layout/Navbar.js
+++ client/src/component/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import { logoutUser } from "../../actions/authActions";
+import { deleteAccount } from "../../actions/profileActions";
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import styled from "styled-components";
@@ -9,6 +10,10 @@ class Navbar extends Component {
 
     onLogoutClick(e) {
         this.props.logoutUser();
+    }
+
+    onDeleteClick(e) {
+        this.props.deleteAccount();
     }
     render() {
         const { isAuthenticated, user } = this.props.auth;
@@ -24,12 +29,34 @@ class Navbar extends Component {
                                     style={{ width: '25px', height: '25px', marginRight: '5px', marginTop: '5px' }}
                                     title="You must have a Gravatar connected to your email to display an image"
                             />{' '}
-                            <a
-                                href="/profile"
-                                className="nav-link"
-                            >
-                                <Text>마이페이지</Text>
-                            </a>
+                        <li className="nav-item dropdown">
+                            <MyPage className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                마이페이지
+                            </MyPage>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <DropDown1 className="dropdown-item" href="#">
+                                    <div>
+                                        <img
+                                            className="rounded-circle"
+                                            src={user.avatar}
+                                            alt={user.name}
+                                            style={{ width: '25px', marginRight: '5px' }}
+                                            title="display an image"
+                                        />
+                                        {user.name}
+                                    </div>
+                                    <Email>{user.email}</Email>
+                                </DropDown1>
+                                <div className="dropdown-divider"></div>
+                                <DropDown2
+                                    className="dropdown-item"
+                                    onClick={this.onDeleteClick.bind(this)}
+                                >
+                                    회원 탈퇴
+                                </DropDown2>
+                            </div>
+                        </li>
                         <a
                             href="/"
                             onClick={this.onLogoutClick.bind(this)}
@@ -96,6 +123,34 @@ const Title = styled.div`
   font-weight: 700;
 `;
 
+const MyPage = styled.a`
+  font-size: 13px;
+  font-weight: 600;
+  display: flex;
+`;
+
+const DropDown1 = styled.div`
+  font-size: 13px;
+`;
+
+const DropDown2 = styled.button`
+  font-size: 13px;
+  color: white;
+  background: rgb(255, 107, 107);
+  border-width: initial;
+  border-style: none;
+  border-color: initial;
+  border-image: initial;
+  border-radius: 4px;
+`;
+
+const Email = styled.div`
+  line-height: 18px;
+  color: #a6a6a6;
+  font-size: 14px;
+  padding-left: 30px;
+`;
+
 Navbar.propTypes = {
     logoutUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired
@@ -105,4 +160,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, { logoutUser })(Navbar);
+export default connect(mapStateToProps, { logoutUser, deleteAccount })(Navbar);
