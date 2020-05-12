0418500a51c25d6a253a39d3ae1305b7aa650bb8
Doowankim <rlaendhks11@gmail.com>
2020-05-12 21:09:51 +0900

    feat: post comment api

--- model/post.js
+++ model/post.js
@@ -21,7 +21,33 @@ const postSchema = mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
-    }
+    },
+    comments: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            title: {
+                type: String,
+                required: true
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
 });
 
 module.exports = mongoose.model('post', postSchema);
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -23,7 +23,7 @@ router.post('/', authCheck, (req, res) => {
         .catch(err => res.json(err));
 });
 
-// @route GET localhost:3040/post/total
+// @route GET localhost:3040/posts/total
 // @desc get posts
 // @access Private & Public
 router.get('/total', (req, res) => {
@@ -39,7 +39,7 @@ router.get('/total', (req, res) => {
         .catch(err => res.json(err));
 });
 
-// @route GET localhost:3040/post/:postId
+// @route GET localhost:3040/posts/:postId
 // @desc get detail posts
 // @access Private
 router.get('/:postId', authCheck, (req, res) => {
@@ -56,6 +56,60 @@ router.get('/:postId', authCheck, (req, res) => {
             }
         })
         .catch(err => res.json(err));
+});
+
+// @route POST localhost:3040/posts/comment/:postId
+// @desc add comment to post
+// @access Private
+router.post('/comment/:postId', authCheck, (req, res) => {
+    const {errors, isValid} = validatePostInput(req.body);
+    if (!isValid) {
+        return res.json(errors);
+    }
+    postModel
+        .findById(req.params.postId)
+        .then(post => {
+            if(!post) {
+                return res.json({ msg: 'no post' });
+            }
+
+            const newComment = {
+                title: req.body.title,
+                text: req.body.text,
+                name: req.user.name,
+                avatar: req.user.avatar,
+                user: req.user.id
+            };
+            post.comments.unshift(newComment);
+            post
+                .save()
+                .then(post => res.json(post));
+        })
+        .catch(err => res.json(err));
+});
+
+// @route POST localhost:3040/posts/comment/:postId/:commentId
+// @desc delete comment from post
+// @access Private
+router.delete('/comment/:postId/:commentId', authCheck, (req, res) => {
+    postModel
+        .findById(req.params.postId)
+        .then(post => {
+            if(post.comments.filter(comment => comment._id.toString() === req.params.commentId).length === 0) {
+                return res.status(400).json({ msg: 'Comment does not exist' });
+            }
+            //삭제(배열)
+            const removeIndex = post.comments
+                .map(item => item._id.toString()) //_id를 toString으로 바꿔줌
+                .indexOf(req.params.commentId)
+
+            post.comments.splice(removeIndex, 1);
+            post
+                .save()
+                .then(post => {
+                    res.json(post)
+                });
+        });
 });
 
 module.exports = router;
