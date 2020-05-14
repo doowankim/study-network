e958cc4ea3b30e7744c02f779d7348ead39db165
Doowankim <rlaendhks11@gmail.com>
2020-05-14 13:00:03 +0900

    fix: login push nextpage path

--- client/src/component/auth/Login.js
+++ client/src/component/auth/Login.js
@@ -18,13 +18,13 @@ class Login extends Component {
 
     componentDidMount() {
         if (this.props.auth.isAuthenticated) {
-            this.props.history.push('/dashboard');
+            this.props.history.push('/');
         }
     }
 
     componentWillReceiveProps(nextProps) {
         if (nextProps.auth.isAuthenticated) {
-            this.props.history.push('/dashboard');
+            this.props.history.push('/');
         }
 
         if (nextProps.errors) {
