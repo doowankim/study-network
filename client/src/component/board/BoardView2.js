7618543d220f796ffb5780497fbb4652887e1a33
Doowankim <rlaendhks11@gmail.com>
2020-06-15 23:52:00 +0900

    feat: BoardView Hook form

---
+++ client/src/component/board/BoardView2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import {useSelector} from 'react-redux';
+import {getPosts} from '../../actions/postActions';
+import BoardList from './BoardList';
+
+const BoardView2 = () => {
+  return (
+    <div>
+      <div>BoardView2 Page</div>
+    </div>
+  );
+};
+
+export default BoardView2;
